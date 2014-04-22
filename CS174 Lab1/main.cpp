d0f29b8fffca0a972169ed1aec70053cb7677fc1
Calvin <calvin.chan.h@gmail.com>
2014-04-21 22:52:23 -0700

    Initial Commit

---
+++ CS174 Lab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS174 Lab1
+//
+//  Created by Calvin on 4/21/14.
+//  Copyright (c) 2014 Calvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
